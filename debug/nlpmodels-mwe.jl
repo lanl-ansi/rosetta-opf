a202cf3cea8d4cd5337d3d8453cea75435664317
Carleton Coffrin <carleton@coffrin.com>
2022-03-23 18:44:13 -0600

    clean up mwe

--- debug/nlpmodels-mwe.jl
+++ debug/nlpmodels-mwe.jl
@@ -1,26 +1,7 @@
-time_start = time()
+# https://juliasmoothoptimizers.github.io/ADNLPModels.jl/stable/tutorial/
 
-using PowerModels
-using NLPModels
 using ADNLPModels
 using NLPModelsIpopt
-
-pkg_load_time = time() - time_start
-
-
-time_start = time()
-
-file_name = "../data/pglib_opf_case5_pjm.m"
-
-data = PowerModels.parse_file(file_name)
-PowerModels.standardize_cost_terms!(data, order=2)
-PowerModels.calc_thermal_limits!(data)
-ref = PowerModels.build_ref(data)[:it][:pm][:nw][0]
-
-data_load_time = time() - time_start
-
-
-time_start = time()
 
 f(x) = (x[1] - 1)^2 + 100*(x[2] - x[1]^2)^2
 
@@ -33,29 +14,9 @@ ucon = [0.75; 0.3]
 lcon = [0.0; 0.0]
 
 nlp = ADNLPModel(f, x0, lvar, uvar, c, lcon, ucon)
-#ADNLPModel(f, x0, lvar, uvar)
-
-
-model_build_time = time() - time_start
-
-
-time_start = time()
 
 #output = ipopt(nlp, print_level=0)
 output = ipopt(nlp)
-cost = output.objective
+
+println(output.objective)
 println(output.solution)
-
-solve_time = time() - time_start
-
-
-println("")
-println("\033[1mSummary\033[0m")
-println("   case......: $(file_name)")
-println("   cost......: $(round(Int, cost))")
-println("   pkg time..: $(pkg_load_time)")
-println("   data time.: $(data_load_time)")
-println("   build time: $(model_build_time)")
-println("   solve time: $(solve_time)")
-println("")
-
