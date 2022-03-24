42d75068e66c204b7949053b70db96114c8374d6
Carleton Coffrin <carleton@coffrin.com>
2022-03-23 20:55:40 -0600

    add basic cli interface

---
+++ rosetta-opf-cli.jl
@@ -0,0 +1,48 @@
+#!/usr/bin/env julia
+
+using ArgParse
+
+s = ArgParseSettings()
+
+@add_arg_table s begin
+    "--solver", "-s"
+        help = "solver to load"
+        required = true
+    "--file", "-f"
+        help = "the opf data file (.m)"
+        required = true
+end
+
+args = parse_args(s)
+
+
+if args["solver"] == "galacticoptim"
+    include("galacticoptim.jl")
+elseif args["solver"] == "jump"
+    include("jump.jl")
+elseif args["solver"] == "nlpmodels"
+    include("nlpmodels.jl")
+elseif args["solver"] == "nonconvex"
+    include("nonconvex.jl")
+elseif args["solver"] == "optim"
+    include("optim.jl")
+else
+    error("unknwon solver type $(args["solver"])")
+end
+
+
+result = solve_opf(args["file"])
+
+
+data = [
+    "DATA",
+    result["case"],
+    result["feasible"],
+    result["cost"],
+    result["time_total"],
+    result["time_data"],
+    result["time_build"],
+    result["time_solve"],
+]
+println(join(data, ", "))
+
