8f6f9beea4fec19511a87a12f4b4cd2ed8a1b8db
Oscar Dowson <odow@users.noreply.github.com>
2022-03-29 14:00:20 +1300

    Add a script for JuMP with SymbolicAD (#8)
    * Add a script for JuMP with SymbolicAD
    Co-authored-by: Carleton Coffrin <carleton@coffrin.com>

---
+++ variants/jump-symbolic-ad.jl
@@ -0,0 +1,139 @@
+#!/usr/bin/env julia
+###### AC-OPF using JuMP ######
+#
+# implementation reference: https://github.com/lanl-ansi/PowerModelsAnnex.jl/blob/master/src/model/ac-opf.jl
+# This uses https://github.com/odow/SymbolicAD.jl instead of the built-in AD
+# library
+
+import PowerModels
+import Ipopt
+import JuMP
+import SymbolicAD
+
+function solve_opf(file_name)
+    time_data_start = time()
+
+    data = PowerModels.parse_file(file_name)
+    PowerModels.standardize_cost_terms!(data, order=2)
+    PowerModels.calc_thermal_limits!(data)
+    ref = PowerModels.build_ref(data)[:it][:pm][:nw][0]
+
+    data_load_time = time() - time_data_start
+
+
+    time_model_start = time()
+
+    model = JuMP.Model(Ipopt.Optimizer)
+    #JuMP.set_optimizer_attribute(model, "print_level", 0)
+
+    JuMP.@variable(model, va[i in keys(ref[:bus])])
+    JuMP.@variable(model, ref[:bus][i]["vmin"] <= vm[i in keys(ref[:bus])] <= ref[:bus][i]["vmax"], start=1.0)
+
+    JuMP.@variable(model, ref[:gen][i]["pmin"] <= pg[i in keys(ref[:gen])] <= ref[:gen][i]["pmax"])
+    JuMP.@variable(model, ref[:gen][i]["qmin"] <= qg[i in keys(ref[:gen])] <= ref[:gen][i]["qmax"])
+
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= p[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+    JuMP.@variable(model, -ref[:branch][l]["rate_a"] <= q[(l,i,j) in ref[:arcs]] <= ref[:branch][l]["rate_a"])
+
+    JuMP.@objective(model, Min, sum(gen["cost"][1]*pg[i]^2 + gen["cost"][2]*pg[i] + gen["cost"][3] for (i,gen) in ref[:gen]))
+
+    for (i,bus) in ref[:ref_buses]
+        JuMP.@constraint(model, va[i] == 0)
+    end
+
+    for (i,bus) in ref[:bus]
+        bus_loads = [ref[:load][l] for l in ref[:bus_loads][i]]
+        bus_shunts = [ref[:shunt][s] for s in ref[:bus_shunts][i]]
+
+        JuMP.@constraint(model,
+            sum(p[a] for a in ref[:bus_arcs][i]) ==
+            sum(pg[g] for g in ref[:bus_gens][i]) -
+            sum(load["pd"] for load in bus_loads) -
+            sum(shunt["gs"] for shunt in bus_shunts)*vm[i]^2
+        )
+
+        JuMP.@constraint(model,
+            sum(q[a] for a in ref[:bus_arcs][i]) ==
+            sum(qg[g] for g in ref[:bus_gens][i]) -
+            sum(load["qd"] for load in bus_loads) +
+            sum(shunt["bs"] for shunt in bus_shunts)*vm[i]^2
+        )
+    end
+
+    # Branch power flow physics and limit constraints
+    for (i,branch) in ref[:branch]
+        f_idx = (i, branch["f_bus"], branch["t_bus"])
+        t_idx = (i, branch["t_bus"], branch["f_bus"])
+
+        p_fr = p[f_idx]
+        q_fr = q[f_idx]
+        p_to = p[t_idx]
+        q_to = q[t_idx]
+
+        vm_fr = vm[branch["f_bus"]]
+        vm_to = vm[branch["t_bus"]]
+        va_fr = va[branch["f_bus"]]
+        va_to = va[branch["t_bus"]]
+
+        g, b = PowerModels.calc_branch_y(branch)
+        tr, ti = PowerModels.calc_branch_t(branch)
+        tm = tr^2 + ti^2
+        g_fr = branch["g_fr"]
+        b_fr = branch["b_fr"]
+        g_to = branch["g_to"]
+        b_to = branch["b_to"]
+
+        # From side of the branch flow
+        JuMP.@NLconstraint(model, p_fr ==  (g+g_fr)/tm*vm_fr^2 + (-g*tr+b*ti)/tm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/tm*(vm_fr*vm_to*sin(va_fr-va_to)) )
+        JuMP.@NLconstraint(model, q_fr == -(b+b_fr)/tm*vm_fr^2 - (-b*tr-g*ti)/tm*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/tm*(vm_fr*vm_to*sin(va_fr-va_to)) )
+
+        # To side of the branch flow
+        JuMP.@NLconstraint(model, p_to ==  (g+g_to)*vm_to^2 + (-g*tr-b*ti)/tm*(vm_to*vm_fr*cos(va_to-va_fr)) + (-b*tr+g*ti)/tm*(vm_to*vm_fr*sin(va_to-va_fr)) )
+        JuMP.@NLconstraint(model, q_to == -(b+b_to)*vm_to^2 - (-b*tr+g*ti)/tm*(vm_to*vm_fr*cos(va_fr-va_to)) + (-g*tr-b*ti)/tm*(vm_to*vm_fr*sin(va_to-va_fr)) )
+
+        # Voltage angle difference limit
+        JuMP.@constraint(model, va_fr - va_to <= branch["angmax"])
+        JuMP.@constraint(model, va_fr - va_to >= branch["angmin"])
+
+        # Apparent power limit, from side and to side
+        JuMP.@constraint(model, p_fr^2 + q_fr^2 <= branch["rate_a"]^2)
+        JuMP.@constraint(model, p_to^2 + q_to^2 <= branch["rate_a"]^2)
+    end
+
+    model_build_time = time() - time_model_start
+
+
+    time_solve_start = time()
+    JuMP.set_optimize_hook(model, SymbolicAD.optimize_hook)
+    JuMP.optimize!(model)
+    cost = JuMP.objective_value(model)
+    feasible = (JuMP.termination_status(model) == JuMP.LOCALLY_SOLVED)
+
+    solve_time = time() - time_solve_start
+    total_time = time() - time_data_start
+
+    println("")
+    println("\033[1mSummary\033[0m")
+    println("   case........: $(file_name)")
+    println("   feasible....: $(feasible)")
+    println("   cost........: $(round(Int, cost))")
+    println("   total time..: $(total_time)")
+    println("     data time.: $(data_load_time)")
+    println("     build time: $(model_build_time)")
+    println("     solve time: $(solve_time)")
+    println("")
+
+    return Dict(
+        "case" => file_name,
+        "feasible" => feasible,
+        "cost" => cost,
+        "time_total" => total_time,
+        "time_data" => data_load_time,
+        "time_build" => model_build_time,
+        "time_solve" => solve_time,
+    )
+end
+
+if isinteractive() == false
+    solve_opf("$(@__DIR__)/../data/pglib_opf_case5_pjm.m")
+end
