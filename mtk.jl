22fa75b200f9d44318dc176c0fefc29293538657
Vaibhav Dixit <vaibhavyashdixit@gmail.com>
2023-08-12 15:31:16 +0530

    Updates

--- mtk.jl
+++ mtk.jl
@@ -7,7 +7,7 @@
 
 import PowerModels
 import Ipopt
-import ModelingToolkit, Optimization, OptimizationMOI
+using ModelingToolkit, Optimization, OptimizationMOI
 import ModelingToolkit: ≲
 
 function solve_opf(file_name)
@@ -22,13 +22,31 @@ function solve_opf(file_name)
 
 
     time_model_start = time()
-    vars = []
+    vars = Num[]
+    lb = Float64[]
+    ub = Float64[]
 
     ModelingToolkit.@variables va[keys(ref[:bus])]
-    ModelingToolkit.@variables vm[keys(ref[:bus])] = ones(length(ref[:bus])) (bounds = [(ref[:bus][i]["vmin"], ref[:bus][i]["vmax"]) for i in keys(ref[:bus])])
+    for i in keys(ref[:bus])
+        push!(lb, -Inf)
+        push!(ub, Inf)
+    end
+    ModelingToolkit.@variables vm[keys(ref[:bus])]
+    for i in keys(ref[:bus])
+        push!(lb, ref[:bus][i]["vmin"])
+        push!(ub, ref[:bus][i]["vmax"])
+    end
     vars = vcat(vars, [va[i] for i in keys(ref[:bus])], [vm[i] for i in keys(ref[:bus])])
-    ModelingToolkit.@variables pg[keys(ref[:gen])] (bounds = [(ref[:gen][i]["pmin"], ref[:gen][i]["pmax"]) for i in keys(ref[:gen])])
-    ModelingToolkit.@variables qg[keys(ref[:gen])] (bounds = [(ref[:gen][i]["qmin"], ref[:gen][i]["qmax"]) for i in keys(ref[:gen])])
+    ModelingToolkit.@variables pg[keys(ref[:gen])]
+    for i in keys(ref[:gen])
+        push!(lb, ref[:gen][i]["pmin"])
+        push!(ub, ref[:gen][i]["pmax"])
+    end
+    ModelingToolkit.@variables qg[keys(ref[:gen])]
+    for i in keys(ref[:gen])
+        push!(lb, ref[:gen][i]["qmin"])
+        push!(ub, ref[:gen][i]["qmax"])
+    end
     vars = vcat(vars, [pg[i] for i in keys(ref[:gen])], [qg[i] for i in keys(ref[:gen])])
     i_inds, j_inds, l_inds = maximum(first.(ref[:arcs])), maximum(getindex.(ref[:arcs], Ref(2))), maximum(last.(ref[:arcs]))
     ModelingToolkit.@variables p[1:i_inds, 1:j_inds, 1:l_inds]
@@ -36,7 +54,14 @@ function solve_opf(file_name)
 
     for (l, i, j) in ref[:arcs]
         push!(vars, p[l, i, j])
-        ModelingToolkit.setmetadata(p[l, i, j], ModelingToolkit.VariableBounds, (-ref[:branch][l]["rate_a"], ref[:branch][l]["rate_a"]))
+        push!(lb, -ref[:branch][l]["rate_a"])
+        push!(ub, ref[:branch][l]["rate_a"])
+    end
+
+    for (l, i, j) in ref[:arcs]
+        push!(vars, q[l, i, j])
+        push!(lb, -ref[:branch][l]["rate_a"])
+        push!(ub, ref[:branch][l]["rate_a"])
     end
 
     loss = sum(gen["cost"][1] * pg[i]^2 + gen["cost"][2] * pg[i] + gen["cost"][3] for (i, gen) in ref[:gen])
@@ -52,8 +77,8 @@ function solve_opf(file_name)
         push!(cons,
             sum(p[a...] for a in ref[:bus_arcs][i]) ~
                 (sum(pg[g] for g in ref[:bus_gens][i]; init = 0.0)) -
-                (sum(load["pd"] for load in bus_loads; init = 0.0))
-            + sum(shunt["gs"] for shunt in bus_shunts; init = 0.0)*vm[i]^2
+                (sum(load["pd"] for load in bus_loads; init = 0.0)) -
+             sum(shunt["gs"] for shunt in bus_shunts; init = 0.0)*vm[i]^2
         )
 
         push!(cons,
@@ -96,8 +121,7 @@ function solve_opf(file_name)
         push!(cons, q_to ~ -(b + b_to) * vm_to^2 - (-b * tr + g * ti) / ttm * (vm_to * vm_fr * cos(va_to - va_fr)) + (-g * tr - b * ti) / ttm * (vm_to * vm_fr * sin(va_to - va_fr)))
 
         # Voltage angle difference limit
-        push!(cons, branch["angmin"] ≲ (va_fr - va_to))
-        push!(cons, (va_fr - va_to) ≲ branch["angmax"])
+        push!(cons, abs(va_fr - va_to) ≲ branch["angmax"])
 
         # Apparent power limit, from side and to side
         push!(cons, p_fr^2 + q_fr^2 ≲ branch["rate_a"]^2)
@@ -109,61 +133,72 @@ function solve_opf(file_name)
 
 
     time_solve_start = time()
-
     optsys = ModelingToolkit.OptimizationSystem(loss, vars, [], constraints=cons, name=:rosetta)
     optsys = ModelingToolkit.structural_simplify(optsys)
-    u0map = Dict([vars[i] => 0.0 for i in 1:length(vars)])
+    u0map = Dict([k => 0.0 for k in collect(optsys.states)])
     for key in keys(ref[:bus])
-        u0map[vm[key]] = 1.0
+        if vm[key] in keys(u0map)
+            u0map[vm[key]] = 1.0
+        end
     end
-    prob = Optimization.OptimizationProblem(optsys, u0map, grad=true, hess=true, cons_j=true, cons_h=true, cons_sparse=true, sparse=true)
+
+    inds = Int[]
+    for k in collect(optsys.states)
+        push!(inds, findall(x -> isequal(x, k), vars)[1])
+    end
+    prob = Optimization.OptimizationProblem(optsys, u0map, lb = lb[inds], ub = ub[inds], grad=true, hess=true, cons_j=true, cons_h=true, cons_sparse=true, sparse=true)
     sol = OptimizationMOI.solve(prob, Ipopt.Optimizer())
     solve_time = time() - time_solve_start
     total_time = time() - time_data_start
 
     println("time_solve ", solve_time)
     println("time_total ", total_time)
-    return sol
+
     # total_callback_time =
-    #     nlp_block.evaluator.eval_objective_timer +
-    #     nlp_block.evaluator.eval_objective_gradient_timer +
-    #     nlp_block.evaluator.eval_constraint_timer +
-    #     nlp_block.evaluator.eval_constraint_jacobian_timer +
-    #     nlp_block.evaluator.eval_hessian_lagrangian_timer
+    #     sol.cache.evaluator.eval_objective_timer +
+    #     sol.cache.evaluator.eval_objective_gradient_timer +
+    #     sol.cache.evaluator.eval_constraint_timer +
+    #     sol.cache.evaluator.eval_constraint_jacobian_timer +
+    #     sol.cache.evaluator.eval_hessian_lagrangian_timer
 
-    # println("")
-    # println("\033[1mSummary\033[0m")
-    # println("   case........: $(file_name)")
-    # println("   variables...: $(model_variables)")
-    # println("   constraints.: $(model_constraints)")
-    # println("   feasible....: $(feasible)")
-    # println("   cost........: $(round(Int, cost))")
-    # println("   total time..: $(total_time)")
-    # println("     data time.: $(data_load_time)")
-    # println("     build time: $(model_build_time)")
-    # println("     solve time: $(solve_time)")
+    model_variables = length(vars)
+    model_constraints = length(sol.cache.lcons)
+    feasible = (sol.retcode == :LOCALLY_SOLVED)
+    cost = sol.objective
+
+    println("")
+    println("\033[1mSummary\033[0m")
+    println("   case........: $(file_name)")
+    println("   variables...: $(model_variables)")
+    println("   constraints.: $(model_constraints)")
+    println("   feasible....: $(feasible)")
+    println("   cost........: $(round(Int, cost))")
+    println("   total time..: $(total_time)")
+    println("     data time.: $(data_load_time)")
+    println("     build time: $(model_build_time)")
+    println("     solve time: $(solve_time)")
     # println("      callbacks: $(total_callback_time)")
-    # println("")
-    # println("   callbacks time:")
-    # println("   * obj.....: $(nlp_block.evaluator.eval_objective_timer)")
-    # println("   * grad....: $(nlp_block.evaluator.eval_objective_gradient_timer)")
-    # println("   * cons....: $(nlp_block.evaluator.eval_constraint_timer)")
-    # println("   * jac.....: $(nlp_block.evaluator.eval_constraint_jacobian_timer)")
-    # println("   * hesslag.: $(nlp_block.evaluator.eval_hessian_lagrangian_timer)")
-    # println("")
+    println("")
+    println("   callbacks time:")
+    # println("   * obj.....: $(sol.cache.evaluator.eval_objective_timer)")
+    # println("   * grad....: $(sol.cache.evaluator.eval_objective_gradient_timer)")
+    # println("   * cons....: $(sol.cache.evaluator.eval_constraint_timer)")
+    # println("   * jac.....: $(sol.cache.evaluator.eval_constraint_jacobian_timer)")
+    # println("   * hesslag.: $(sol.cache.evaluator.eval_hessian_lagrangian_timer)")
+    println("")
 
-    # return Dict(
-    #     "case" => file_name,
-    #     "variables" => model_variables,
-    #     "constraints" => model_constraints,
-    #     "feasible" => feasible,
-    #     "cost" => cost,
-    #     "time_total" => total_time,
-    #     "time_data" => data_load_time,
-    #     "time_build" => model_build_time,
-    #     "time_solve" => solve_time,
-    #     "time_callbacks" => total_callback_time,
-    # )
+    return Dict(
+        "case" => file_name,
+        "variables" => model_variables,
+        "constraints" => model_constraints,
+        "feasible" => feasible,
+        "cost" => cost,
+        "time_total" => total_time,
+        "time_data" => data_load_time,
+        "time_build" => model_build_time,
+        "time_solve" => solve_time,
+        # "time_callbacks" => total_callback_time,
+    )
 end
 
 if isinteractive() == false
