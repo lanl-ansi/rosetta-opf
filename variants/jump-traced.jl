f8cd9dd13945bad9a7b07592fa556117b9dbd565
Oscar Dowson <odow@users.noreply.github.com>
2024-01-22 09:52:11 +1300

    Update variants/jump-traced.jl

--- variants/jump-traced.jl
+++ variants/jump-traced.jl
@@ -399,5 +399,5 @@ function solve_opf(file_name)
 end
 
 if isinteractive() == false
-    solve_opf("$(@__DIR__)/data/pglib_opf_case5_pjm.m")
+    solve_opf("$(@__DIR__)/../data/pglib_opf_case5_pjm.m")
 end
