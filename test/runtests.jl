079eaeb763bfc14943233cff3e43b3b4050fb4c2
Oscar Dowson <odow@users.noreply.github.com>
2024-01-21 21:32:59 +1300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -167,12 +167,12 @@ end
     ]
         include(joinpath(dirname(@__DIR__), "$framework.jl"))
         @testset "$case" for case in [
-            "opf_warmup",
-            "case5_pjm",
-            "case14_ieee",
-            "case24_ieee_rts",
+            "opf_warmup.m",
+            "pglib_opf_case5_pjm.m",
+            "pglib_opf_case14_ieee.m",
+            "pglib_opf_case24_ieee_rts.m",
         ]
-            test_case = joinpath(dirname(@__DIR__), "data/pglib_opf_$case.m")
+            test_case = joinpath(dirname(@__DIR__), "data", case)
             result = solve_opf(test_case)
             validate_result(test_case, result)
         end
