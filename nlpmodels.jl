45dea2d56fae45de64d193e69dc8ed25843dd149
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-06-22 14:17:05 -0400

    [ADNLPModels] Upgrade to v0.8.2

--- nlpmodels.jl
+++ nlpmodels.jl
@@ -16,7 +16,6 @@ import PowerModels
 import ADNLPModels
 import ConcreteStructs
 import NLPModelsIpopt
-import Symbolics
 
 ConcreteStructs.@concrete struct DataRepresentation
     data
@@ -541,4 +540,3 @@ end
 if isinteractive() == false
     solve_opf("$(@__DIR__)/data/opf_warmup.m")
 end
-
